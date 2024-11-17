4c051e180f3cd92edeec1d5b2e69a0bd4c67e4b5
Jakub <jakubgalazka34@gmail.com>
2024-11-17 15:28:06 +0100

    poprowdadawp;askfc

--- app.js
+++ app.js
@@ -16,69 +16,110 @@ async function wywswietlanie() {
     const p2 = document.getElementById('name2')
     const populacja2 = document.getElementById('populacja2')
     const button = document.getElementById("button")
+    const newGame = document.getElementById("newGame")
+    const punkty = document.getElementById("punkty")
+    const wiekszaPop = document.getElementById("wiekszaPop")
     let goodAnsw = 0
     let badAnsw = 0
     let country
     let population1 = 0 
     let population2 = 0
+    function aktualizacjaPkt(){
+        punkty.textContent = `Poprawne: ${goodAnsw} | Niepoprawne: ${badAnsw}`
+    }
+    function wszPop(){
+        if(population1>population2){
+            wiekszaPop.innerHTML = `Kraj z większą populacją to: ${p1.innerHTML}`
+        }else if(population2>population1){
+            wiekszaPop.innerHTML = `Kraj z większą populacją to: ${p2.innerHTML}`
+        }else{
+            wiekszaPop.innerHTML = "Kraje mają taką samą populację"
+        }
+    }
     function losowanie(){
         function kraj1(){
-        country = Math.floor(Math.random() * countries.length);
-        img1.setAttribute("src", countries[country].flags.png);
-        p1.innerHTML = countries[country].name.common;
-        population1 = countries[country].population
-        console.log(population1);
-        div1.style.backgroundColor = '#f2efcb'
-    }       function kraj2(){
-        country = Math.floor(Math.random() *countries.length)
-        img2.setAttribute("src", countries[country].flags.png);
-        p2.innerHTML = countries[country].name.common;
-        population2 = countries[country].population
-        console.log(population2);
-        div2.style.backgroundColor = "#f2efcb"
+            const country = Math.floor(Math.random() * countries.length);
+            img1.setAttribute("src", countries[country].flags.png);
+            p1.innerHTML = countries[country].name.common;
+            population1 = countries[country].population;
+            div1.style.backgroundColor = '#f2efcb';
+        }
+
+        function kraj2(){
+            let country;
+            do {
+                country = Math.floor(Math.random() * countries.length);
+            } while (countries[country].name.common === p1.innerHTML);
+            img2.setAttribute("src", countries[country].flags.png);
+            p2.innerHTML = countries[country].name.common;
+            population2 = countries[country].population;
+            div2.style.backgroundColor = "#f2efcb";
+        }
+        
+        kraj1();
+        kraj2();
     }
-    kraj1()    
-    kraj2()
-    if(p1.innerHTML==p2.innerHTML){
-    kraj1()    
-    kraj2()
-    }
-    }
-    losowanie()
+
     function klikanie(){
         div1.addEventListener("click", function(){
-            if(population1>population2){
-                goodAnsw++
-                div1.style.backgroundColor = 'green'
-                populacja1.innerHTML = `Population: ${population1}`
-                populacja2.innerHTML = `Population: ${population2}`
-            }else if(population2>population1){
-                badAnsw++
-                div1.style.backgroundColor = 'red'
-                populacja1.innerHTML = `Population: ${population1}`
-                populacja2.innerHTML = `Population: ${population2}`
+            if(population1 > population2){
+                goodAnsw++;
+                div1.style.backgroundColor = 'green';
+            } else {
+                badAnsw++;
+                div1.style.backgroundColor = 'red';
             }
+            populacja1.innerHTML = `Population: ${population1}`;
+            populacja2.innerHTML = `Population: ${population2}`;
+            aktualizacjaPkt()
+            wszPop()
+            sprawdzKoniecGry();
+        });
+
         div2.addEventListener("click", function(){
-            if(population2>population1){
-                goodAnsw++
-                div2.style.backgroundColor = 'green'
-                populacja1.innerHTML = `Population: ${population1}`
-                populacja2.innerHTML = `Population: ${population2}`
-            }else if(population1>population2){
-                badAnsw++
-                div2.style.backgroundColor= 'red'
-                populacja1.innerHTML = `Population: ${population1}`
-                populacja2.innerHTML = `Population: ${population2}`
+            if(population2 > population1){
+                goodAnsw++;
+                div2.style.backgroundColor = 'green';
+            } else {
+                badAnsw++;
+                div2.style.backgroundColor = 'red';
             }
-        })
-        })
+            populacja1.innerHTML = `Population: ${population1}`;
+            populacja2.innerHTML = `Population: ${population2}`;
+            aktualizacjaPkt()
+            wszPop()
+            sprawdzKoniecGry();
+        });
     }
-    klikanie()
+
+    function sprawdzKoniecGry(){
+        if(badAnsw >= 5){
+            alert("Gra skończona! Przegrałeś 5 razy.");
+            button.disabled = true;
+        }
+    }
+
     button.addEventListener("click", function(){
+        losowanie();
+        populacja1.innerHTML = '';
+        populacja2.innerHTML = '';
+        div1.style.backgroundColor = '#f2efcb';
+        div2.style.backgroundColor = '#f2efcb';
+    });
+    newGame.addEventListener("click", function(){
+        goodAnsw=0
+        badAnsw=0
+        button.disabled = false;
+        populacja1.innerHTML=" "
+        populacja2.innerHTML=" "
+        div1.style.backgroundColor = '#f2efcb';
+        div2.style.backgroundColor = '#f2efcb';
         losowanie()
-        populacja1.innerHTML = ''
-        populacja2.innerHTML = ''
     })
+
+    losowanie();
+    klikanie();
+    aktualizacjaPkt()
 }
 wywswietlanie()
 //zrobic wieksza populacje w li
